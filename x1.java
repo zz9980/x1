44797e861f5027012f19d4f733480fca937dc81c
Bruce Alan Martin <bam@suffolk.li>
2015-09-09 00:55:56 -0400

    Starter code for x1

---
+++ x1.java
@@ -0,0 +1,55 @@
+//////// What am I trying to do???
+//////// What is my name?  (CST 112; today's date?)
+
+//// GLOBALS:  coordinates, speed, etc.
+float x, y;       // Position of creature.
+float dx, dy;     // Speed.
+float horizon;
+
+//// SETUP:  window size, initialization (start in middle of screen).
+void setup() {
+  size( 640,480);
+  horizon=  height/4;
+  x=  width/2;
+  y=  height/2;
+  dx=  3;
+  dy=  2;
+}
+
+//// NEXT FRAME:  scene, action, show.
+void draw() {
+  //// SCENE:  sky, sun, tree, house, etc.
+  background( 100,150,200 );                // sky
+  fill( 255,255,0 );
+  ellipse( width*3/4, height/8, 40,40 );    // sun
+  // Grass
+  fill( 100,200,100 );
+  rect( 0,horizon, width,height*3/4 );      // grass.
+  
+  /* INSERT YOUR CODE HERE! */
+                                            // tree
+                                            // house                                            
+                                            
+  //// ACTION:  move (x,y) coordinates.
+  x=  x + dx;
+  y=  y + dy;
+  
+  //// SHOW:  display the creature at (x,y)
+
+  /* INSERT YOUR CODE HERE! */
+  fill(255,0,0); rect( x,y, 30,50 );        /* REPLACE THIS WITH YOUR OWN CODE! */
+  text( "Fred", x,y );
+
+}
+
+
+//////// HANDLERS:  mouse clicks, keys
+void mousePressed() {
+  x=  mouseX;
+  y=  mouseY;
+  //
+  dx=  random( -6, +6 );
+  dy=  random( -4, +4 );
+}
+
+
